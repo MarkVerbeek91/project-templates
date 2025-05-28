4cb2c66db66907b2fe153b87f8c32db5f01c746b
M. Verbeek <mark.verbeek@thermofisher.com>
2025-05-28 20:10:16 +0200

    feat(main): reworked the entry point functions, not usefull yet

--- src/run/__init__.py
+++ src/run/__init__.py
@@ -2,42 +2,54 @@ import shutil
 from pathlib import Path
 from copier import run_copy
 
-workspace_dir = Path(__file__).parent.parent.parent
 
-template_dir = workspace_dir / Path("template_workspace")
-destination = workspace_dir / Path("target")
+WORKSPACE_DIR = Path(__file__).parent.parent.parent
 
-shutil.rmtree(destination, ignore_errors=True)
+WORKSPACE_TEMPLATE_DIR = WORKSPACE_DIR / Path("python-workspace-template")
+PROJECT_TEMPLATE_DIR = WORKSPACE_DIR / Path("python-project-template")
+TARGET_DIR = WORKSPACE_DIR / Path("target")
 
-answers = {
-    "workspace_name": "Sample-Workspace",
-    "module_name": "sample_module_main",
-    "packages": [
-        {
-            "project_name": "Sample01",
-            "description": "Sample package description",
-            "module_name": "sample_module_01",
-            "package_name": "sample_package_01",
-        },
-        {
-            "project_name": "Sample02",
-            "description": "Sample package description",
-            "module_name": "sample_module_02",
-            "package_name": "sample_package_02",
-        },
-    ],
-}
+shutil.rmtree(TARGET_DIR, ignore_errors=True)
 
-# Create a project from a local path
-run_copy(str(template_dir), str(destination), data=answers)
 
-workspace_name = str(answers["workspace_name"])
-destination = destination / workspace_name / "packages"
+def create_workspace():
+    """Create a workspace with sample packages."""
 
-for package in list(answers["packages"]):
-    package_dir = destination / package["project_name"]
-
-    template_dir = workspace_dir / Path("template_project")
+    answers = {
+        "workspace_name": "Sample-Workspace",
+        "module_name": "sample_module_main",
+        "packages": [
+            {
+                "project_name": "Sample01",
+                "description": "Sample package description",
+                "module_name": "sample_module_01",
+                "package_name": "sample_package_01",
+            },
+            {
+                "project_name": "Sample02",
+                "description": "Sample package description",
+                "module_name": "sample_module_02",
+                "package_name": "sample_package_02",
+            },
+        ],
+    }
 
     # Create a project from a local path
-    run_copy(str(template_dir), str(destination), data=package)
+    run_copy(str(WORKSPACE_TEMPLATE_DIR), str(TARGET_DIR), data=answers)
+
+    workspace_name = str(answers["workspace_name"])
+    TARGET_DIR = TARGET_DIR / workspace_name / "packages"
+
+    for package in list(answers["packages"]):
+        package_dir = TARGET_DIR / package["project_name"]
+
+        WORKSPACE_TEMPLATE_DIR = WORKSPACE_DIR / Path("template_project")
+
+        # Create a project from a local path
+        run_copy(str(WORKSPACE_TEMPLATE_DIR), str(TARGET_DIR), data=package)
+
+
+def create_project():
+    """Create a project with sample packages."""
+    pass
+
